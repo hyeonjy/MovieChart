c286361781cf72d751fe01024a8bd1bbfd3ff3b4
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2024-10-16 09:52:53 +0900

    feat: 아이콘 클릭시 검색 input 토글 추가

---
+++ js/search.js
@@ -0,0 +1,20 @@
+const searchContainer = document.querySelector(".search-wrap");
+const searchIcon = document.querySelector(".search-icon");
+const searchInput = document.getElementById("search-input");
+
+let isSearchOpen = false;
+
+searchIcon.addEventListener("click", () => {
+  isSearchOpen = !isSearchOpen;
+  searchContainer.classList.toggle("active");
+});
+
+//input 외부요소 클릭시 닫힘
+function handleClickOuter(event) {
+  if (isSearchOpen && !searchContainer.contains(event.target)) {
+    isSearchOpen = false;
+    searchContainer.classList.toggle("active");
+  }
+}
+
+document.addEventListener("click", handleClickOuter);
