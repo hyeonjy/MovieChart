8b54e4c8a0f83b71fb5a5681b02e98bf2cade867
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2024-10-22 12:22:45 +0900

    refactor: 슬라이더 설정을 slider.js로 이동

--- js/movie.js
+++ js/movie.js
@@ -1,4 +1,5 @@
 import config from "./apikey.js";
+import { initializeSwiper } from "./slider.js";
 
 export const BASE_PATH = "https://api.themoviedb.org/3";
 
@@ -55,46 +56,6 @@ function renderMovies(movies, title, index) {
 
   sectionList.innerHTML = swiperHtml;
   initializeSwiper(index);
-}
-
-function initializeSwiper(index) {
-  const config = {
-    container: `#swiper${index}`,
-    next: `#next${index}`,
-    prev: `#prev${index}`,
-  };
-
-  const swiper = new Swiper(config.container, {
-    slidesPerView: 1,
-    spaceBetween: 100,
-    navigation: {
-      nextEl: config.next,
-      prevEl: config.prev,
-    },
-    // autoplay: {
-    //   delay: 3000,
-    //   disableOnInteraction: false,
-    // },
-    loop: true,
-    loopAdditionalSlides: 0,
-    breakpoints: {
-      600: {
-        slidesPerView: 2,
-        spaceBetween: 100,
-        slidesPerGroup: 2,
-      },
-      1024: {
-        slidesPerView: 4,
-        spaceBetween: 20,
-        slidesPerGroup: 4,
-      },
-      1200: {
-        slidesPerView: 5,
-        spaceBetween: 20,
-        slidesPerGroup: 5,
-      },
-    },
-  });
 }
 
 async function displayMovies() {
--- js/slider.js
+++ js/slider.js
@@ -1,53 +1,39 @@
-export function initializeSwipers() {
-  const swipersConfig = [
-    {
-      container: "#swiper1",
-      next: "#next1",
-      prev: "#prev1",
-    },
-    {
-      container: "#swiper2",
-      next: "#next2",
-      prev: "#prev2",
-    },
-  ];
+export function initializeSwiper(index) {
+  const config = {
+    container: `#swiper${index}`,
+    next: `#next${index}`,
+    prev: `#prev${index}`,
+  };
 
-  swipersConfig.forEach((config) => {
-    new Swiper(config.container, {
-      slidesPerView: 1,
-      spaceBetween: 100, //아이템 사이 간격
-      navigation: {
-        nextEl: config.next,
-        prevEl: config.prev,
+  const swiper = new Swiper(config.container, {
+    slidesPerView: 1,
+    spaceBetween: 100,
+    navigation: {
+      nextEl: config.next,
+      prevEl: config.prev,
+    },
+    // autoplay: {
+    //   delay: 3000,
+    //   disableOnInteraction: false,
+    // },
+    loop: true,
+    loopAdditionalSlides: 0,
+    breakpoints: {
+      600: {
+        slidesPerView: 2,
+        spaceBetween: 100,
+        slidesPerGroup: 2,
       },
-      // autoplay: {
-      //   // 자동 슬라이드 설정
-      //   delay: 3000,
-      //   disableOnInteraction: false,
-      // },
-      loop: true,
-      breakpoints: {
-        500: {
-          slidesPerView: 2,
-          spaceBetween: 50,
-          slidesPerGroup: 2,
-        },
-        800: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-          slidesPerGroup: 3,
-        },
-        1024: {
-          slidesPerView: 4,
-          spaceBetween: 20,
-          slidesPerGroup: 4,
-        },
-        1200: {
-          slidesPerView: 5,
-          spaceBetween: 20,
-          slidesPerGroup: 5,
-        },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        slidesPerGroup: 4,
       },
-    });
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+        slidesPerGroup: 5,
+      },
+    },
   });
 }
